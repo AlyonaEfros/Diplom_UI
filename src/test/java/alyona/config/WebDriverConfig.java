9e68ff84d824d59da87abbf471f81bee2cf4da8f
AlyonaEfros <AlyonaEfros@yandex.ru>
2023-06-13 15:48:51 +0300

    add test

--- src/test/java/alyona/config/WebDriverConfig.java
+++ src/test/java/alyona/config/WebDriverConfig.java
@@ -2,9 +2,6 @@ package alyona.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({
-        "classpath:alyona.${env}.properties"
-})
 public interface WebDriverConfig extends Config {
     @Key("browserName")
     @DefaultValue("chrome")
