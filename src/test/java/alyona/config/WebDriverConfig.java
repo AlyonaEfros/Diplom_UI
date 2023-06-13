71370c93dad776693a7271b91ec3f3b34bb515a8
AlyonaEfros <AlyonaEfros@yandex.ru>
2023-06-13 17:11:38 +0300

    build

--- src/test/java/alyona/config/WebDriverConfig.java
+++ src/test/java/alyona/config/WebDriverConfig.java
@@ -1,7 +1,9 @@
 package alyona.config;
 
 import org.aeonbits.owner.Config;
-
+@Config.Sources({
+        "classpath:${env}.properties"
+})
 public interface WebDriverConfig extends Config {
 
     @Key("browserName")
